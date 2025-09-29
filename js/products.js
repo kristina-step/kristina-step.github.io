7bbeff3349c3ede8d0258510272dad6aa903a3aa
kristina-step <157066665+kristina-step@users.noreply.github.com>
2025-09-29 20:46:26 +0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -2,22 +2,43 @@ const products = [
     {
         id: 1,
         name: "Boing 737",
-        price: 29999,
+        price: 98000000, 
         image: "boeing737.jpg",
-        description: "Самолет"
+        description: "Классика пассажирской авиации"
     },
     {
         id: 2,
         name: "Boeing 777",
-        price: 54999,
+        price: 105000000, 
         image: "boeing777.jpg",
-        description: "Отличный самолет - модерн"
+        description: "Современный авиаборт"
     },
     {
-        id: 2,
+        id: 3,
         name: "Boeing 747",
-        price: 54999,
+        price: 107000000, 
         image: "boeing747.jpg",
-        description: "Отличный самолет - масштаб"
+        description: "Лучший спонсор грузоперевозок"
+    },
+    {
+        id: 4,
+        name: "Boeing 737-800",
+        price: 86000000, 
+        image: "boeing737-800.jpg",
+        description: "Лучшее сочетание цены и качества"
+    },
+    {
+        id: 5,
+        name: "Boeing 787",
+        price: 104000000, 
+        image: "boeing787.jpg",
+        description: "Лучшая вместимость"
+    },
+    {
+        id: 6,
+        name: "Boeing 757",
+        price: 52000000, 
+        image: "boeing757.jpg",
+        description: "Неизменный золотой стандарт"
     },
 ];
